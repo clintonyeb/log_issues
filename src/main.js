6a9c2ac2f0552ed81c910aaddf90a1ccf424c893
Ashish Menghani <ashish.men@gmail.com>
2017-11-15 16:19:07 +0530

    Adding support for websocket

--- src/main.js
+++ src/main.js
@@ -6,10 +6,17 @@ import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
 import VueSession from 'vue-session'
+import VueNativeSock from 'vue-native-websocket'
 
 Vue.use(VueSession)
 Vue.use(BootstrapVue)
 Vue.use(VueResource)
+Vue.use(VueNativeSock, 'ws://ec2-18-220-63-112.us-east-2.compute.amazonaws.com:8080/Javemonitor/datachannel/log:74af15fa-a797-438a-a4ef-54a8de6ca4ef:213', {
+  format: 'json',
+  reconnection: true, // (Boolean) whether to reconnect automatically (false)
+  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+  reconnectionDelay: 3000 // (Number) how long to initially wait before attempting a new (1000)
+})
 
 Vue.http.options.xhr = {withCredentials: true}
 Vue.http.options.emulateJSON = true
@@ -20,6 +27,7 @@ Vue.http.headers.common['Access-Control-Allow-Origin'] = '*'
 Vue.http.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
 Vue.http.headers.common['Accept'] = 'application/json, text/plain, */*'
 Vue.http.headers.common['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, Authorization, Access-Control-Allow-Origin'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
